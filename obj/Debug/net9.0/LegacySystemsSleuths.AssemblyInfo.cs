57a2ff92f54bcc617405bbad7dd38c1bb5b3a408
Joel Southall <vextechmage@gmail.com>
2026-05-30 01:30:33 -0700

    Add main site pages

--- obj/Debug/net9.0/LegacySystemsSleuths.AssemblyInfo.cs
+++ obj/Debug/net9.0/LegacySystemsSleuths.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegacySystemsSleuths")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf5ea036539e990749deb96c13b53a6ad2ad539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5fecdaa214d76d62d32873d39629047097b448")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegacySystemsSleuths")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegacySystemsSleuths")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
