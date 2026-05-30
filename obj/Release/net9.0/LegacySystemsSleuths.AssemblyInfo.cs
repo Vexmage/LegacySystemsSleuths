4ae89057e31575b7641bcf60e6d17ea11293f1f9
Joel Southall <vextechmage@gmail.com>
2026-05-30 03:00:39 -0700

    Build first working Legacy Systems Sleuths site

--- obj/Release/net9.0/LegacySystemsSleuths.AssemblyInfo.cs
+++ obj/Release/net9.0/LegacySystemsSleuths.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LegacySystemsSleuths")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a5fecdaa214d76d62d32873d39629047097b448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e267c3db70570fdb85a9636b4f4bf564360dbbaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("LegacySystemsSleuths")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LegacySystemsSleuths")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
